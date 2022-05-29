53090500a59354b9035bc7b1a30eca965abfb5a1
Bartekxseo <62757822+Bartekxseo@users.noreply.github.com>
2022-05-29 19:49:54 +0200

    Migration

---
+++ PT.DataAccess/Migrations/202205291749190_AddPhotoAndPhotoListToAnimal.cs
@@ -0,0 +1,32 @@
+﻿namespace PT.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddPhotoAndPhotoListToAnimal : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Photo",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Path = c.String(),
+                        FileName = c.String(),
+                        AnimalId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Animal", t => t.AnimalId)
+                .Index(t => t.AnimalId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Photo", "AnimalId", "dbo.Animal");
+            DropIndex("dbo.Photo", new[] { "AnimalId" });
+            DropTable("dbo.Photo");
+        }
+    }
+}
